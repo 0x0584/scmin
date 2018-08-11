7c0dc35025e8fcf67d8ea5a2daf9275f2ce10ffa
Anas Rchid <rchid.anas@gmail.com>
2018-08-11 03:27:52 +0100

    fix bugs in vector
    it was calling memset() after calling free() on a void ** object, the
    solution was just calling free() because it's a 1d array even that
    it's void **

--- src/vector.c
+++ src/vector.c
@@ -5,14 +5,13 @@ vector_t *vector_new(void) {
 
     v->capacity = 0;
     v->size = 0;
-    v->objs = NULL;
+    v->objs = malloc(sizeof(object_t));
 
     return v;
 }
 
 void vector_free(vector_t * v) {
     free(v->objs);
-    memset(v->objs, 0, v->capacity * sizeof (object_t));
     free(v);
 }
 
@@ -21,23 +20,28 @@ void vector_compact(vector_t * v) {
 
     for (i = 0; i < size; ++i) {
 	if (!(v->objs[i])) {
-	    for (j = i; j < size; ++j) {
-		v->objs[i] = v->objs[i + 1];
+	    for (j = i; j < size - 1; ++j) {
+		v->objs[j] = v->objs[j + 1];
 	    }
 	    --size;
 	}
     }
 
     v->size = size;
+    v->capacity = size;
+
+    v->objs = realloc(v->objs, size * sizeof(object_t));
 }
 
 void vector_add(vector_t * v, object_t o) {
     if (v->size == v->capacity) {
-	const int vdc = VECTOR_DEFAULT_CAPACITY;
+	const int dc = VECTOR_DEFAULT_CAPACITY;
+	const int oc = v->capacity;	/* old capacity */
 
-	v->capacity += vdc;
-	v->objs = realloc(v->objs, v->capacity * sizeof(object_t));
-	/* memset(v->objs + vdc, 0, vdc * sizeof(object_t)); */
+	v->objs = realloc(v->objs, (oc + dc) * sizeof(object_t));
+	memset(v->objs + oc, 0, dc * sizeof(object_t));
+
+	v->capacity = oc + dc;
     }
 
     v->objs[v->size] = o;
@@ -49,7 +53,6 @@ void vector_del(vector_t * v, int i) {
 	return;
     }
 
-    free(v->objs[i]);
     v->objs[i] = NULL;
     v->size--;
 }
@@ -76,8 +79,6 @@ void vector_set(vector_t * v, int i, object_t o) {
     if (i < 0 || i > v->size) {
 	return;
     }
-
-    free(v->objs[i]);
     v->objs[i] = o;
 }
 
@@ -92,10 +93,12 @@ object_t vector_get(vector_t * v, int i) {
 #if defined VECTOR_DEBUG
 void vector_debug(FILE * stream, vector_t * v) {
     int i;
-    
+
+    puts("--------------");
+    fprintf(stream, "[size:%d] [capacity:%d]\n", v->size, v->capacity);
     puts("--------------");
     for (i = 0; i < v->capacity; ++i) {
-	fprintf(stream, "[%d] = %p\n", i, v->objs[i]);
+	fprintf(stream, "[%d] - %p \n", i , v->objs[i]);
     }
     puts("--------------");
 }
@@ -104,14 +107,33 @@ void vector_testing(void) {
     vector_t *v = vector_new();
     int i, size = 30, tab[size];
 
+
+    for (i = 0; i < size; ++i) {
+	tab[i] = 2 * i;
+    }
+
     for (i = 0; i < size; ++i) {
 	vector_add(v, &tab[i]);
     }
 
     vector_debug(stdout, v);
-    vector_free(v);
-
-    if(!v) puts("ss");
     
+    for (i = 0; i < v->size; ++i) {
+	fprintf(stdout, "[%d] = %d\n", i, *((int *)v->objs[i]));
+    }
+    puts("--------------");
+    
+    for (i = 0; i < size; ++i) {
+	if (i % 3 == 0) {
+	    vector_del(v, i);
+	}
+    }
+
+    vector_debug(stdout, v);
+
+    vector_compact(v);
+    vector_debug(stdout, v);
+    
+    vector_free(v);
 }
 #endif
